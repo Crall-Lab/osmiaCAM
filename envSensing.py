b63666609b757a7f1c8252b9aa42d9a7952558ff
James Crall <james.crall@gmail.com>
2025-02-20 23:33:30 -0600

    updating env sensing

--- envSensing.py
+++ envSensing.py
@@ -17,17 +17,16 @@ from time import sleep
 inner = adafruit_ahtx0.AHTx0(board.I2C())
 outer = MS8607(board.I2C())
 
-while True:
-    t = datetime.datetime.now()
-    ds = t.strftime("%Y-%m-%d %H:%M:%S")
-    outer_meas = "," + "%.2f" % outer.pressure + "," + "%.2f" % outer.temperature + "," + "%.2f" % atm.relative_humidity
-    inner_meas = "," + "%.2f" % inner.temperature + "," + "%.2f" % inner.relative_humidity + "\n"
-    print(ds+outer_meas+inner_meas)
-    
-    #Write measurements
-    with open("/mnt/bumblebox/data/envLogger.csv", "a") as f:
-					f.write(ds+outer_meas+inner_meas)
-    print("Data recorded!")
-    
-    #Wait for a minute before taking another measurement
-    #sleep(59)
+t = datetime.datetime.now()
+ds = t.strftime("%Y-%m-%d %H:%M:%S")
+outer_meas = "," + "%.2f" % outer.pressure + "," + "%.2f" % outer.temperature + "," + "%.2f" % atm.relative_humidity
+inner_meas = "," + "%.2f" % inner.temperature + "," + "%.2f" % inner.relative_humidity 
+print(ds+outer_meas+inner_meas)
+
+#Write measurements
+with open("/mnt/bumblebox/data/envLogger.csv", "a") as f:
+				f.write(ds+outer_meas+inner_meas+"\n")
+print("Data recorded!")
+
+#Wait for a minute before taking another measurement
+#sleep(59)

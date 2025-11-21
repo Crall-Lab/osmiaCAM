16cdddf1fa67897c2a0a86038ff309b4f4a36b3b
acaciatang <tst116@ic.ac.uk>
2025-11-20 19:08:38 -0600

    equalise, all jsons

--- extCamAnalysis.py
+++ extCamAnalysis.py
@@ -6,36 +6,55 @@ import glob
 import os
 
 """
-Only tested with one single labelme JSON.
-Run from code directory held within the same parent as data directory and 'Results' directory.
+Run on all data.
+Untested, please fine tune parameters (Commented, dark/light difference, mostly. If equalising is not working well try not hardcoding, or using percentage of "light" instead. This will make sense in context, maybe. Slack me if not.).
 """
 
-#labels
-#labels = json.load(open('../Osmia_cameras/osmia3/OsmiaVids/extCam/05_09_25/osmia3_2025-05-09.json'))
-#nests = labels['shapes']
-#filename = '../Osmia_cameras/osmia3/OsmiaVids/extCam/05_09_25/osmia3_2025-05-09_08-00-01_ext1.h264'
+JSONfolder = '/Volumes/crall2/Crall_Lab/osmia_2025/oCAM_ROIs_CA2025' #Change me as needed, but if you change the folder structure be prepared to go on an adventure.
 
-def oneVid(filename, outDir, nests, write=False):
+def oneVid(filename, outDir, jsonDir, write=False):
+    """
+    Analyse one video. fine tune me.
+    """
+    out = None #csv
+
     print(filename)
-    #output
-    out = None
+    bookmark = os.path.basename(filename).replace('h264', 'z')
+
+
+    
+    jsons = os.listdir(jsonDir) + [bookmark]
+    jsons.sort()
+
+    if jsons.index(bookmark) == 0:
+        print('Missing ROI file for ' + filename)
+        return None
+    
+    labels = json.load(open(os.path.join(jsonDir, jsons[jsons.index(bookmark)-1])))
+    nests = labels['shapes']
     
     #read video
     cap = cv2.VideoCapture(filename)
     if write:
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
         outVid = cv2.VideoWriter(os.path.join(outDir, os.path.basename(filename).replace('.h264', 'bees_.mp4')), fourcc, 30.0, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),  int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
     f = 0
     while cap.isOpened():
         print(f)
-        ret, frame = cap.read()
+        ret, raw = cap.read()
     
-        # if frame is read correctly ret is True
         if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
+            print("End of video.")
             break
         
-        #change in plans, find bees based on darker colour. They like to stay too much. The black line is annoying but fine
+        gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+        frame = cv2.equalizeHist(gray) #equalise globally
+        
+        #For local
+        #clahe = cv.createCLAHE(tileGridSize=(8,8)) #play with grid size, if poor performance play with 
+        #frame = clahe.apply(gray)
+        
+        #find bees based on darker colour, scanning along x-axis.
         cnt = 0
         for n in nests:
             [x1, y1], [x2, y2] = n['points']
@@ -43,10 +62,10 @@ def oneVid(filename, outDir, nests, write=False):
             ys = [int(y1),int(y2)]
             xs.sort()
             ys.sort()
-            crop = frame[ys[0]:ys[1], xs[0]:xs[1]][:,:,0]
-            light = np.percentile(crop, 90, axis = 1)
-            mid = np.percentile(crop, 50, axis = 1)
-            bee = light-mid > 90
+            crop = frame[ys[0]:ys[1], xs[0]:xs[1]]
+            light = np.percentile(crop, 90, axis = 1) #percentile to be "light"
+            mid = np.percentile(crop, 50, axis = 1) #bee, basically
+            bee = light-mid > 90 #how much darker is bee, probably change this
             toTrue = [i for i in range(1,len(bee)) if bee[i] and not bee[i-1]]
             toFalse = [i for i in range(len(bee)-1) if bee[i] and not bee[i+1]]
             if bee[0] == True:
@@ -61,17 +80,17 @@ def oneVid(filename, outDir, nests, write=False):
             if len(toTrue) == 1:
                 ymax = toFalse[0]
                 ymin = toTrue[0]
-                if ymax-ymin > 50:
+                if ymax-ymin > 50: #size of bee
                     print('Found one!')
-                    row = pd.DataFrame([[filename, f, cnt,  ys[0]+ymin+(ymax-ymin)/2]])
+                    row = pd.DataFrame([[filename, f, cnt, ys[0]+ymin, ys[0]+ymax, ys[0]+ymin+(ymax-ymin)/2]]) #'filename', 'frame', 'nestLabel', 'beeStart', , 'beeEnd', 'centroid'
                     if write:
-                        cv2.rectangle(frame,(xs[0], ys[0]+ymin),(xs[1], ys[0]+ymax),(0,255,0),3)
+                        cv2.rectangle(raw,(xs[0], ys[0]+ymin),(xs[1], ys[0]+ymax),(0,255,0),3)
 
                 cnt += 1
                 continue
 
             i=0
-            while i < len(toTrue):
+            while i < len(toTrue): #more than on dark patch
                 if toFalse[i]-toTrue[i] < 50:
                     i += 1
                     pass
@@ -79,12 +98,12 @@ def oneVid(filename, outDir, nests, write=False):
                     print('Found one!')
                     while i < len(toTrue)-1:
                         ymin = toTrue[i]
-                        if toFalse[i]-toTrue[i+1] < 50:
+                        if toFalse[i]-toTrue[i+1] < 50:  #size of bee
                             i += 1
                         ymax = toFalse[i]
-                        row = pd.DataFrame([[filename, f, cnt,  ys[0]+ymin+(ymax-ymin)/2]])
+                        row = pd.DataFrame([[filename, f, cnt, ys[0]+ymin, ys[0]+ymax, ys[0]+ymin+(ymax-ymin)/2]])
                         if write:
-                            cv2.rectangle(frame,(xs[0], ys[0]+ymin),(xs[1], ys[0]+ymax),(0,255,0),3)
+                            cv2.rectangle(raw,(xs[0], ys[0]+ymin),(xs[1], ys[0]+ymax),(0,255,0),3)
                         
                         if out is None:
                             out = row
@@ -97,7 +116,7 @@ def oneVid(filename, outDir, nests, write=False):
             cnt += 1
         
         if write:
-            outVid.write(frame)
+            outVid.write(raw)
         f += 1
         
         if cv2.waitKey(1) == ord('q'):
@@ -108,7 +127,7 @@ def oneVid(filename, outDir, nests, write=False):
     cv2.destroyAllWindows()
     
     if out is not None:
-        out.columns = ('filename', 'frame', 'nestLabel', 'centroid')
+        out.columns = ('filename', 'frame', 'nestLabel', 'beeStart', , 'beeEnd', 'centroid')
         out.to_csv(os.path.join(outDir,os.path.basename(filename).replace('.h264', '_obv.csv')), index=False)
     else:
         noneOut = pd.DataFrame(columns = ['path', 'start', 'end', 'nestLabel', 'dir'])
@@ -116,28 +135,25 @@ def oneVid(filename, outDir, nests, write=False):
     return out
 
 #folder
-def runDay(folder, outDir):
+def runDay(folder, outDir, jsonDir):
+    """
+    Run one camera-day. Should be fine unless you want to change how in and out is decided.
+    """
+
     cnt=0
-    jsonName = glob.glob(os.path.join(folder, '*.json'))
-    #take me out
-    if len(jsonName) == 0:
-        jsonName = '../Osmia_cameras/osmia3/OsmiaVids/extCam/05_09_25/osmia3_2025-05-09.json'
-    else:
-        jsonName = jsonName[0]
-    #
-    labels = json.load(open(jsonName))
-    nests = labels['shapes']
+
     for filename in glob.glob(os.path.join(folder,'*.h264')):
         if os.path.isfile(os.path.join(outDir, os.path.basename(filename).replace('h264', 'csv'))):
             print('Done, skipping')
             continue
         oneOut = None
         if cnt%10 == 0: #write every 10th
-            oneIn = oneVid(filename, outDir, nests, True)
+            oneIn = oneVid(filename, outDir, jsonDir, True)
         else:
-            oneIn = oneVid(filename, outDir, nests, False)
+            oneIn = oneVid(filename, outDir, jsonDir, False)
         cnt += 1
         if oneIn is None:
+            print('OUTPUT EMPTY')
             continue
         for n in set(oneIn.nestLabel):
             subset = oneIn[oneIn['nestLabel'] == n]
@@ -152,10 +168,10 @@ def runDay(folder, outDir):
             #centroids = list(subset.centroid)
             dir = []
             for i in range(len(end)):
-                vector = subset[subset['frame'] == end[i]].centroid[0]-subset[subset['frame'] == start[i]].centroid[0]
-                if vector == 0: #top left is 0,0
+                vector = subset[subset['frame'] == end[i]].centroid[0]-subset[subset['frame'] == start[i]].centroid[0]  #top left is 0,0
+                if vector == 0:
                     dir.append('still')
-                elif vector < 0:
+                elif vector < 0:  #any movement up
                     dir.append('in')
                 else:
                     dir.append('out')
@@ -178,9 +194,19 @@ def runDay(folder, outDir):
         out.to_csv(os.path.join(outDir, os.path.basename(folder)+'.csv'), index=False)
 
 
-for folder in glob.glob('../Osmia_cameras/osmia3/OsmiaVids/extCam/*'):
-    base = os.path.basename(folder)
-    outDir = '../Results/' + base
-    if not os.path.exists(outDir):
-        os.mkdir(outDir)
-    runDay(folder, outDir)
+for folder in glob.glob('/Volumes/crall2/Crall_Lab/osmia_2025/OsmiaCam_Data/Osmia_cameras/*'): #Change the folder structure if (and only if) you want to try string manipulation. It'll be fun, or maybe not but hey, YOLO
+    if os.path.isdir(folder):
+        base = os.path.basename(folder)
+        jsonDir = os.path.join(JSONfolder, base+'_ROI')
+        if not os.path.exists('Results'):
+            os.mkdir('Results')
+        if os.path.exists(jsonDir):
+            outDir = 'Results/' + base #Results will be in wd.
+            if not os.path.exists(outDir):
+                os.mkdir(outDir)
+
+            for day in glob.glob(os.path.join(folder, 'OsmiaVids', 'extCam', '*')): #change if starting lower
+                
+                runDay(day, outDir, jsonDir)
+        else:
+            print('Where are the ROI files for '+base+'?')

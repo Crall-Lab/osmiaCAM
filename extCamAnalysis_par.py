1079f12e2a906eb1aad3dfd4f42b974346d76673
Acacia <72441149+acaciatang@users.noreply.github.com>
2025-11-26 18:46:54 -0600

    bug

--- extCamAnalysis_par.py
+++ extCamAnalysis_par.py
@@ -133,7 +133,8 @@ def oneVid(filename, outDir, jsonDir, write=False):
             break
 
     cap.release()
-    outVid.release()
+    if write:
+        outVid.release()
     cv2.destroyAllWindows()
     
     if out is not None:

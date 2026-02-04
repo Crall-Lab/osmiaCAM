3f6a40ae4bfb0dcc7fbe65cbc3294b71a6b3e2aa
acaciatang <tst116@ic.ac.uk>
2026-02-04 15:47:56 -0600

    nest for validation

--- extCamAnalysis.py
+++ extCamAnalysis.py
@@ -10,18 +10,11 @@ Run on all data.
 Untested, please fine tune parameters (Commented, dark/light difference, mostly. If equalising is not working well try not hardcoding, or using percentage of "light" instead. This will make sense in context, maybe. Slack me if not.).
 """
 
-""" 
-Environment installation
-conda create -n osmiaCam python=3.12.2
-conda activate osmiaCam
-pip3 install matplotlib==3.10.7 numpy==2.2.5 opencv.python==4.12.0.88 pandas==2.2.3 #used to say pip which will not work for mac
-"""
+jsonDir = '/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/extROI' #Change me as needed, but if you change the folder structure be prepared to go on an adventure.
+vidDir = '/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/osmia4/'
+outMainDir = '/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/Results/ext'
 
-JSONfolder = '/Volumes/crall2/Crall_Lab/osmia_2025/oCAM_ROIs_CA2025' #Change me as needed, but if you change the folder structure be prepared to go on an adventure.
-vidDir = '/Volumes/crall2/Crall_Lab/osmia_2025/oCAM_subset_test/Osmia_cameras/*'
-outMainDir = '/Volumes/crall2/Crall_Lab/osmia_2025/Results_subset_29112025'
-
-def oneVid(filename, outDir, jsonDir):
+def oneVid(filename, outDir, jsonDir, write=True):
     """
     Analyse one video. fine tune me.
     """
@@ -40,73 +33,108 @@ def oneVid(filename, outDir, jsonDir):
     labels = json.load(open(os.path.join(jsonDir, jsons[jsons.index(bookmark)-1])))
     nests = labels['shapes']
     
+
     #read video
     cap = cv2.VideoCapture(filename)
+    if write:
+        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+        outVid = cv2.VideoWriter(os.path.join(outDir, os.path.basename(filename).replace('.h264', 'bees_.mp4')), fourcc, 30.0, (int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)),  int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))))
     f = 0
     while cap.isOpened():
-        cnt = 0
-        holder = []
-        while cnt < 25:
-            ret, raw = cap.read()
-            if not ret:
-                print("End of video.")
-                break
-            
-            gray = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)#equalise globally
-
-            holder.append(cv2.equalizeHist(gray))
-            cnt += 1
+        #print(f)
+        ret, raw = cap.read()
+    
+        if not ret:
+            print("End of video.")
+            break
         
-        stack = np.stack(holder, axis=2)
+        frame = cv2.cvtColor(raw, cv2.COLOR_BGR2GRAY)
+
+        ## remove shadow, from https://opencv.org/blog/shadow-correction-using-opencv
+        #dilated_img = cv2.dilate(gray, np.ones((7,7), np.uint8))
+        #bg_img = cv2.medianBlur(dilated_img, 21)
+        #diff_img = 255 - cv2.absdiff(gray, bg_img)
+        #frame = cv2.normalize(diff_img,None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
+
+        #frame = cv2.equalizeHist(noShadow) #equalise globally
+
+        #For local
+        #clahe = cv.createCLAHE(tileGridSize=(8,8)) #play with grid size, if poor performance play with 
+        #frame = clahe.apply(gray)
         
         #find bees based on darker colour, scanning along x-axis.
+        cnt = 0
         for n in nests:
             [x1, y1], [x2, y2] = n['points']
             xs = [int(x1),int(x2)]
             ys = [int(y1),int(y2)]
             xs.sort()
             ys.sort()
-            crop = stack[ys[0]:ys[1], xs[0]:xs[1],:]
+            crop = frame[ys[0]:ys[1], xs[0]:xs[1]]
             light = np.percentile(crop, 90, axis = 1) #percentile to be "light"
             mid = np.percentile(crop, 50, axis = 1) #bee, basically
             bee = light-mid > 90 #how much darker is bee, probably change this
-            bee = bee.astype('int')
-            difference = np.diff(bee, prepend=False, append=False, axis=0)
-            toTrue = pd.DataFrame(np.where(difference == 1)).T
-            toTrue.columns = ['beeStart', 'frame']
-            toTrue.sort_values(['frame', 'beeStart'], inplace=True, ignore_index=True)
-            toFalse = pd.DataFrame(np.where(difference == -1)).T
-            toFalse.columns = ['beeEnd', 'frame2']
-            toFalse.sort_values(['frame2', 'beeEnd'], inplace=True, ignore_index=True)
+            toTrue = [i for i in range(1,len(bee)) if bee[i] and not bee[i-1]]
+            toFalse = [i for i in range(len(bee)-1) if bee[i] and not bee[i+1]]
+            if bee[0] == True:
+                toTrue = [0]+toTrue
+            if bee[-1] == True:
+                toFalse += [len(bee)]
 
-            df = pd.concat([toTrue, toFalse], axis=1)
-            df.drop('frame2', axis=1, inplace=True)
-            df.frame += f
-            df['beeSize'] = df.beeEnd-df.beeStart
-            df['filename'] = filename
-            df['nestLabel'] = n['label']
-            df['x0'] = xs[0]
-            df['x1'] = xs[1]
-            df['centroidY'] = ys[0]+df.beeStart+(df.beeEnd-df.beeStart)/2
+            if len(toTrue) == 0:
+                cnt += 1
+                continue
 
-            maximums = df[['frame', 'beeSize']].groupby('frame')['beeSize'].idxmax().values
-            oneBee = df.loc[maximums]
-            
-            oneBee = oneBee[oneBee['beeSize'] > 50]
+            if len(toTrue) == 1:
+                ymax = toFalse[0]
+                ymin = toTrue[0]
+                if ymax-ymin > 50: #size of bee
+                    print('Found one!')
+                    row = pd.DataFrame([[filename, f, cnt, ys[0]+ymin, ys[0]+ymax, ys[0]+ymin+(ymax-ymin)/2]]) #'filename', 'frame', 'nestLabel', 'beeStart', , 'beeEnd', 'centroid'
+                    if write:
+                        cv2.rectangle(raw,(xs[0], ys[0]+ymin),(xs[1], ys[0]+ymax),(0,255,0),3)
 
-            if out is None:
-                out = oneBee
-            else:
-                out = pd.concat([out, oneBee], ignore_index=True)
+                cnt += 1
+                continue
 
-        f += stack.shape[2]
-                
+            i=0
+            while i < len(toTrue): #more than on dark patch
+                if toFalse[i]-toTrue[i] < 50:
+                    i += 1
+                    pass
+                else:
+                    print('Found one!')
+                    while i < len(toTrue)-1:
+                        ymin = toTrue[i]
+                        if toFalse[i]-toTrue[i+1] < 50:  #size of bee
+                            i += 1
+                        ymax = toFalse[i]
+                        row = pd.DataFrame([[filename, f, cnt, ys[0]+ymin, ys[0]+ymax, ys[0]+ymin+(ymax-ymin)/2]])
+                        if write:
+                            cv2.rectangle(raw,(xs[0], ys[0]+ymin),(xs[1], ys[0]+ymax),(0,255,0),3)
+                        
+                        if out is None:
+                            out = row
+                            break
+                        else:
+                            out = pd.concat([out, row])
+                            break
+                    i += 1
+                    break #should not need this but alas
+            cnt += 1
+        
+        if write:
+            outVid.write(raw)
+        f += 1
+        
         if cv2.waitKey(1) == ord('q'):
             break
 
     cap.release()
+    if write:
+        outVid.release()
     cv2.destroyAllWindows()
-    
+
     if out is not None:
         out.columns = ('filename', 'frame', 'nestLabel', 'beeStart', 'beeEnd', 'centroid')
         out.to_csv(os.path.join(outDir,os.path.basename(filename).replace('.h264', '_'+str(f)+'_obv.csv')), index=False)
@@ -157,7 +185,6 @@ def write(filename, outDir, out):
 for folder in glob.glob(vidDir): #Change the folder structure if (and only if) you want to try string manipulation. It'll be fun, or maybe not but hey, YOLO
     if os.path.isdir(folder):
         base = os.path.basename(folder)
-        jsonDir = os.path.join(JSONfolder, base+'_ROI')
         if not os.path.exists(outMainDir):
             os.mkdir(outMainDir)
         if os.path.exists(jsonDir):
@@ -170,7 +197,7 @@ for folder in glob.glob(vidDir): #Change the folder structure if (and only if) y
                     if len(glob.glob(os.path.join(outDir, os.path.basename(filename).split('.')[0]+'*'+'.csv'))) != 0:
                         print('Done, skipping')
                         continue
-                    out = oneVid(filename, outDir, jsonDir, True)
+                    out = oneVid(filename, outDir, jsonDir)
                     #if cnt % 10 == 0:
                     #    write(filename, outDir, out)
                     cnt += 1
--- nests.py
+++ nests.py
@@ -3,59 +3,98 @@ import glob
 import pandas as pd
 import datetime
 import numpy as np
+import os
+import cv2
+import json
 
-df = None
+outDir = '/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/nestOut'
+jsonDir = '/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/nestROI'
 
-for f in glob.glob('/Volumes/crall2/Crall_Lab/osmia_2025/Results_nestred_26122025/megachilidae/megachilidae_2025-04-07_07-12-02_nest0/*.csv'):
-    new = pd.read_csv(f)
-    if len(new.index) == 0:
-        continue
+for dir in glob.glob('/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/nestResults/*'):
+    dir = '/Volumes/crall2/Crall_Lab/osmia_2025/Manuscript/nestResults/osmia4_2025-03-27_07-12-02_nest0'
+    jName = os.path.join(jsonDir, os.path.basename(dir)+'.json')
+    labels = json.load(open(jName))
+    nests = labels['shapes']
 
-    if df is None:
-        df = new
+    df = None
+    for f in glob.glob(os.path.join(dir, '*.csv')):
+        new = pd.read_csv(f)
+        if len(new.index) == 0:
+            continue
+
+        if df is None:
+            df = new
+        else:
+            df = pd.concat([df, new], ignore_index=True)
+    
+    if df is not None:
+        split = df['filename'].str.split(pat='/', n= -1, expand=True, regex=None)
+        basename = df['filename'].str.split(pat='/', n= -1, expand=True, regex=None)[split.columns[-1]]
+        splitTime = basename.str.split(pat='_', n= -1, expand=True, regex=None)
+        time = pd.to_datetime(splitTime[1]+ ' ' + splitTime[2], format='%Y-%m-%d %H-%M-%S')
+        df['time'] = time + pd.to_timedelta(df['frame']*(1/15),unit='s')#15Hz
     else:
-        df = pd.concat([df, new], ignore_index=True)
+        print('No bees found:(')
 
-split = df['filename'].str.split(pat='/', n= -1, expand=True, regex=None)
-basename = df['filename'].str.split(pat='/', n= -1, expand=True, regex=None)[split.columns[-1]]
-splitTime = basename.str.split(pat='_', n= -1, expand=True, regex=None)
-time = pd.to_datetime(splitTime[1]+ ' ' + splitTime[2], format='%Y-%m-%d %H-%M-%S')
-df['time'] = time + pd.to_timedelta(df['frame']*4,unit='s')#15Hz
+    #plot all time max
+    #NUM_COLORS = len(set(df['nestLabel']))
+    #cm = plt.get_cmap('gist_rainbow')
+    #colours = [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
+    #fig, ax = plt.subplots()
+
+    for n in set(df['nestLabel']):
+        if not os.path.exists(os.path.join(outDir, str(n))):
+            os.mkdir(os.path.join(outDir, str(n)))
+        for i in nests:
+            if i['label'] == str(n):
+                break
+        [x1, y1], [x2, y2] = i['points']
+
+        subset = df[df['nestLabel'] == n]
+        #subset = all[all.yEnd >100]
+        byTime = subset.set_index(subset.time)
+        byTime = byTime.sort_index(ascending=False)
+        byTime['max'] = byTime['yEnd'].cummax() #yEnd > yStart
+        byTime.index = np.arange(len(byTime))
+
+        stops, count = np.unique(byTime['max'], return_counts = True)
+
+        for c,y in enumerate(stops):
+            if count[c] < 10:
+                continue
+            else:
+                first = np.where(byTime['max']==y)[0].max()
+                frame = cv2.imread(byTime['filename'][first].replace('h264', 'jpg'))
+                
+                cv2.line(frame,(int(x1),y),(int(x2),y),(255,255,0),3)
+                
+                cv2.imwrite(os.path.join(os.path.join(outDir, str(n)), os.path.basename(byTime['filename'][first].replace('h264', 'jpg'))), frame)
+                    
+                
+        #ax.scatter(byTime['time'], byTime['max'], alpha=0.5, color=colours[c])
+        #ax.set(ylim=(0, 1400))
+
+        #ax.legend(set(df['nestLabel']), bbox_to_anchor=(1.1, 1))
+
+
+        #plt.show()
+
 
 #plot all movement
-NUM_COLORS = len(set(df['nestLabel']))
-cm = plt.get_cmap('gist_rainbow')
-colours = [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
+#NUM_COLORS = len(set(df['nestLabel']))
+#cm = plt.get_cmap('gist_rainbow')
+#colours = [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
 
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
 
-for c, n in enumerate(set(df['nestLabel'])):
-    #all = df[df['nestLabel'] == n]
-    #subset = all[all.yEnd >100]
-    subset = df[df['nestLabel'] == n]
-    ax.scatter(subset['time'], subset['yEnd'], alpha=0.5, color=colours[c])
+#for c, n in enumerate(set(df['nestLabel'])):
+#    #all = df[df['nestLabel'] == n]
+#    #subset = all[all.yEnd >100]
+#    subset = df[df['nestLabel'] == n]
+#    ax.scatter(subset['time'], subset['yEnd'], alpha=0.5, color=colours[c])
 
-    ax.legend(set(df['nestLabel']), bbox_to_anchor=(1.1, 1))
-    ax.set(ylim=(0, 1400))
+#    ax.legend(set(df['nestLabel']), bbox_to_anchor=(1.1, 1))
+#    ax.set(ylim=(0, 1400))
 
-plt.show()
-
-#plot all time max
-NUM_COLORS = len(set(df['nestLabel']))
-cm = plt.get_cmap('gist_rainbow')
-colours = [cm(1.*i/NUM_COLORS) for i in range(NUM_COLORS)]
-fig, ax = plt.subplots()
-
-for c, n in enumerate(set(df['nestLabel'])):
-    subset = df[df['nestLabel'] == n]
-    #subset = all[all.yEnd >100]
-    byTime = subset.set_index(subset.time)
-    byTime = byTime.sort_index(ascending=False)
-    byTime['max'] = byTime['yEnd'].cummax() #yEnd > yStart
-    ax.scatter(byTime['time'], byTime['max'], alpha=0.5, color=colours[c])
-    ax.set(ylim=(0, 1400))
-
-    ax.legend(set(df['nestLabel']), bbox_to_anchor=(1.1, 1))
-
-plt.show()
+#plt.show()
 
